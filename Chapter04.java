3cace8b4ff3c5e114faeb27a020b40afc10bb42f
RyoIchikawa <95997394+ryoichikawa1202@users.noreply.github.com@users.noreply.github.com>
2022-05-07 00:14:37 +0900

    5章

---
+++ Chapter04.java
@@ -0,0 +1,12 @@
+public class Chapter04 {
+  public static void main(String[] args) {
+    String name = "Ryo";
+    System.out.println(name);
+
+    name = "Savas";
+    System.out.println(name);
+
+    final int LUCKY_NUMBER = 7;
+    System.out.println(LUCKY_NUMBER);
+  }
+}
