24523746be20968db7a417ba5c34e950a1b6915f
RyoIchikawa <1202.ryo1997@gmail.com>
2022-05-10 08:42:39 +0900

    java4-1,4-2

---
+++ java41.java
@@ -0,0 +1,8 @@
+public class java41 {
+  public static void main(String[] args) {
+    String spam = "SPAM";
+    for (int i = 0; i < 10; i++) {
+      System.out.println(spam);
+    }
+  }
+}
---
+++ java42.java
@@ -0,0 +1,7 @@
+public class java42 {
+  public static void main(String[] args){
+    for (int i = 1; i <=9; i++) {
+      System.out.println(3 * i);
+    }
+  }
+}
