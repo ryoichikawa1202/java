241bc67344042024f660a3b7064185bb85b58281
RyoIchikawa <1202.ryo1997@gmail.com>
2022-05-14 18:48:52 +0900

    9章クラスサンプルyamada

---
+++ ClassMethod01.java
@@ -0,0 +1,6 @@
+public class ClassMethod01 {
+  public static void main(String[] args) {
+    Human01 yamada = new Human01();
+    System.out.println("名前は" + yamada.name + "で、年齢は" + yamada.age + "です");
+  }
+}
