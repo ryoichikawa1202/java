fc44d394dca6f9cf469a5c8509d61ba02661a2fb
RyoIchikawa <1202.ryo1997@gmail.com>
2022-05-15 18:54:50 +0900

    空欄部分

--- forsample.java
+++ forsample.java
@@ -1,5 +1,11 @@
 public class forsample {
   public static void main(String[] args) {
-    
+    for(int i = 0; i < 5; i++) {
+      for(int j = 0; j < i; j++) {
+        System.out.println(" ");
+      }
+
+      
+    }
   }
 }
