eba76b0c7832632fae1f73f67e1bca0469b3ecc8
RyoIchikawa <1202.ryo1997@gmail.com>
2022-05-15 19:30:40 +0900

    完成

--- forsample.java
+++ forsample.java
@@ -2,12 +2,13 @@ public class forsample {
   public static void main(String[] args) {
     for(int i = 0; i < 5; i++) {
       for(int j = 0; j < i; j++) {
-        System.out.println(" ");
+        System.out.print(" ");
       }
 
       for(int k = 0; k < (5 - i) * 2 - 1; k++) {
-        System.out.println("*");
+        System.out.print("*");
       }
+      System.out.println();
     }
   }
 }
