902e133af2c4a134297f971d8cb8369ab10bcc29
RyoIchikawa <1202.ryo1997@gmail.com>
2022-05-17 21:38:55 +0900

    電気代の計算

---
+++ D200.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        int i = Integer.parseInt(line);
+        int x = 1;
+        x = i * 25 * 24;
+        System.out.println(x);
+    }
+}
