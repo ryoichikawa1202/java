af163e9cba6f6d2d7b5c0849e8f2e0a728e7bab0
RyoIchikawa <1202.ryo1997@gmail.com>
2022-05-17 21:42:22 +0900

    カロリーの計算3

--- D194.java
+++ D194.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 
-public class Main {
+public class D194 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
