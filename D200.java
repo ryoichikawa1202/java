bb363db2cd2be0bbdf37b0699a7b8d5f24aa7eca
RyoIchikawa <1202.ryo1997@gmail.com>
2022-05-17 21:40:41 +0900

    電気代の計算2

--- D200.java
+++ D200.java
@@ -1,4 +1,6 @@
-public class Main {
+import java.util.*;
+
+public class D200 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String line = sc.nextLine();
