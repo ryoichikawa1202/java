9529d88aef242a2a0e3f9e6edfab73620ded6f52
RyoIchikawa <1202.ryo1997@gmail.com>
2022-05-18 19:03:00 +0900

    センチをミリ

---
+++ D190.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+
+public class D190 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        int i = Integer.parseInt(line);
+        System.out.println(i * 10);
+    }
+}
