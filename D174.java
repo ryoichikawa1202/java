4a9d481ca8ff2420fe31d6b69903008ac8da2911
RyoIchikawa <1202.ryo1997@gmail.com>
2022-05-18 19:06:47 +0900

    栄養の摂取

---
+++ D174.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+
+public class D174 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        int p = Integer.parseInt(line);
+        System.out.println(p * 1500);
+    }
+}
