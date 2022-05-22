60a7684af95475f8db587f70a5ce7beebe013771
RyoIchikawa <1202.ryo1997@gmail.com>
2022-05-22 21:09:43 +0900

    ゼロ以外

---
+++ ifCD.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+
+public class ifCD {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int line = sc.nextInt();
+        if (line == 0) {
+            System.out.println("NO");
+        } else {
+            System.out.println("YES");
+        }
+        
+    }
+}
