ffedd65fe012800e5dd87db1def2201f8b124f1c
RyoIchikawa <1202.ryo1997@gmail.com>
2022-05-26 22:32:24 +0900

    0 が含まれていないかanswer

--- D5.java
+++ D5.java
@@ -4,12 +4,22 @@ import java.util.*;
 public class D5 {
     public static void main(String[] args) {
       Scanner sc = new Scanner(System.in);
-      int line = sc.nextInt();
-      
-      if (line == 0) {
-          System.out.println("NO");
-      } else {
-          System.out.println("YES");
+      int n = sc.nextInt();
+      int[] a = new int[n];
+      for (int i = 0; i < n; i++) {
+        a[i] = sc.nextInt();
       }
+
+      for (int x : a) {
+        if (x == 0) {
+            System.out.println("NO");
+            sc.close();
+            return;
+        }
+      }
+
+      System.out.println("YES");
+
+        sc.close();
     }
 }
