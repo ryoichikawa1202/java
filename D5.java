40b9c38673df8b5946762a13d204509251283b80
RyoIchikawa <1202.ryo1997@gmail.com>
2022-05-26 22:20:56 +0900

    0 が含まれていないか2

---
+++ D5.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+
+public class D5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println("XXXXXX");
+    }
+}
