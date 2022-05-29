cf63ff795d5ecdf6be781d2bf727fb38d0def004
RyoIchikawa <1202.ryo1997@gmail.com>
2022-05-29 22:16:45 +0900

    AND3

--- AND.java
+++ AND.java
@@ -3,8 +3,14 @@ import java.util.*;
 
 public class AND {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
-        System.out.println("XXXXXX");
+      Scanner sc = new Scanner(System.in);
+      int n = sc.nextInt();
+      int m = sc.nextInt();
+      
+      if (n >= 10 && m >= 10) {
+          System.out.println("YES");
+      } else {
+          System.out.println("NO");
+      }
     }
 }
