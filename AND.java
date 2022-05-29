1b3f73e376a41fb45f4aea6f664b6accf6c9578d
RyoIchikawa <1202.ryo1997@gmail.com>
2022-05-29 22:13:00 +0900

    AND2

---
+++ AND.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+
+public class AND {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println("XXXXXX");
+    }
+}
