ff2b1b60fa01627cf0f8bd570ca54550db328157
RyoIchikawa <1202.ryo1997@gmail.com>
2022-05-30 21:09:40 +0900

    校庭マラソン3

--- D170.java
+++ D170.java
@@ -4,7 +4,9 @@ import java.util.*;
 public class D170 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
-        System.out.println("XXXXXX");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(n * m);
     }
 }
