b9e7a82d477cd590771520a3abbaf1853e1235f5
RyoIchikawa <1202.ryo1997@gmail.com>
2022-06-01 20:56:37 +0900

    大文字判定4

--- javaif.java
+++ javaif.java
@@ -2,17 +2,19 @@ import java.util.*;
 
 
 public class javaif {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String s = sc.next();
 
-        char c = s.charAt(0);
-        if (Character.isUpperCase(c)) {
-            System.out.println("YES");
-        } else {
-            System.out.println("NO");
-        }
+  public static void main(String[] args) {
+      Scanner sc = new Scanner(System.in);
 
-        sc.close();
-    }
+      String s = sc.next();
+
+      char c = s.charAt(0);
+      if (Character.isUpperCase(c)) {
+          System.out.println("YES");
+      } else {
+          System.out.println("NO");
+      }
+
+      sc.close();
+  }
 }
