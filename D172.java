060a6aaea82d1c078396f9f9ac61cd3681943c70
RyoIchikawa <1202.ryo1997@gmail.com>
2022-06-02 19:53:08 +0900

    イブの日付2

---
+++ D172.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+
+public class D172 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int line = sc.nextInt();
+        System.out.println(line - 1);
+    }
+}
